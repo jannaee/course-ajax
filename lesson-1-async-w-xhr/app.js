f956ec8f910376bcde7298818b8b17e809d981d8
Jannaee <jannaee@gmail.com>
2018-07-03 22:02:22 -0400

    refactor: Modifying code to incorporate the setRequestHeader method

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -3,10 +3,32 @@
     const searchField = document.querySelector('#search-keyword');
     let searchedForText;
     const responseContainer = document.querySelector('#response-container');
-
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
     });
+
+    //XHR Request
+    const requestObject = new XMLHttpRequest();//instantiate the xhr obj
+    requestObject.onload = addImage;//If successfull a response is returned
+    requestObject.onerror = function (errorMessage) {
+        console.log('Houston we have a problem');
+        requestError(errorMessage, 'image');
+    };//if not successful this will occur
+
+    requestObject.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedText}`);
+    requestObject.setRequestHeader('', '');
+    requestAnimationFrame.send();
+    const searchedText = 'android';
+    requestObject.send();
+
+    function addImage() {
+        console.log('Successful retrieval of data');
+        let htmlContent = '';
+        const data = JSON.parse(this.responseText);
+        const firstImage = data.results[0];
+        htmlContent = `<figure></figure>`;
+        
+    }
 })();
